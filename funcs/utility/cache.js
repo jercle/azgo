79d37670646163c76028f778d15b62460d4d6357
Evan Colwell <ercolwell@outlook.com>
2022-06-14 13:10:39 +1000

    Created selectActiveSubscription

--- funcs/utility/cache.js
+++ funcs/utility/cache.js
@@ -4,7 +4,7 @@ const os = require("os")
 const inquirer = require("inquirer")
 
 let appCache = {
-  cachePath: "/Users/jercle/git/azure-tooling/testData/.cache/appcache.json",
+  cachePath: "/Users/jercle/git/stkcat/azure-tooling/testData/.cache/appcache.json",
   // cachePath: `${os.homedir()}/.azgo/appcache.json`,
   cache: {},
   init: function (opts) {
