f26706e95b3eefb6d617b937cb864741d4a1c848
Evan Colwell <ercolwell@outlook.com>
2022-06-15 22:30:54 +1000

    Begin build of updateAssessmentDatabase function

--- funcs/az/test-getSubAssessments.js
+++ funcs/az/test-getSubAssessments.js
@@ -7,35 +7,45 @@
  * @return
  */
 
-// const { DefaultAzureCredential } = require("@azure/identity")
+const { DefaultAzureCredential } = require("@azure/identity")
 const { SecurityCenter } = require("@azure/arm-security")
+const { writeFileSync } = require("fs")
 
 const opts = {
-  acrRegistry: "",
-  subscriptionId: "",
-  imageName: "",
-  imageRetention: 30,
-  resourceGroup: "",
-  acrName: "",
+  subscriptionId: process.env.subscriptionId,
+  resourceGroup: process.env.resourceGroup,
+  acrName: process.env.acrName,
+  assessmentName: process.env.assessmentName,
+  testDataPath: process.env.testDataPath,
 }
 
-// getSubAssessments(opts, new DefaultAzureCredential())
+getSubAssessments(opts, new DefaultAzureCredential())
 
 async function getSubAssessments(
-  { assessmentName, subscriptionId, resourceGroup, acrName },
+  { assessmentName, subscriptionId, resourceGroup, acrName, testDataPath },
   credentials
 ) {
   const acrRegistry = `https://${acrName}.azurecr.io`
   const secClient = new SecurityCenter(credentials, subscriptionId)
 
-  const subAssessmentsList = secClient.subAssessments.list(
+  const subAssessmentsList = await secClient.subAssessments.list(
     `/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.ContainerRegistry/registries/${acrName}`,
     assessmentName
   )
+  console.log(subAssessmentsList)
   let subassessments = []
   for await (subAssessment of subAssessmentsList) {
     subassessments = [...subassessments, subAssessment]
   }
+
+  const dateGathered = new Date().toString()
+
+
+
+  writeFileSync(
+    `${testDataPath}/subassessments.json`,
+    JSON.stringify(subassessments)
+  )
   return subassessments
 }
 
