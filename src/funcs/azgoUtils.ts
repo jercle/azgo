14bf8bd997495b783588154b8baa9acd472c82d4
Evan Colwell <ercolwell@outlook.com>
2022-06-28 00:28:04 +1000

    Directory refactor (#7)
    * Moved pre-oclif files to legacy_azgo_files dir, updated deps, moved new oclif generate CLI to base
    
    * Update dirs

---
+++ src/funcs/azgoUtils.ts
@@ -0,0 +1,123 @@
+// TODO: Filter array from array of conditions.
+import { readFileSync, utimes, writeFileSync } from "fs";
+import { arch, platform } from 'os'
+
+import chalk from 'chalk'
+import { MongoClient } from "mongodb";
+import commandExists from 'command-exists'
+
+
+import {
+  transformVulnerabilityData,
+  groupByAttribute,
+  getAllUniqueCves,
+  countByAttribute
+} from './azureVulnarabilityAggregation.js'
+
+
+// const repos = JSON.parse(readFileSync("/Users/jercle/git/azgo/testData/20220616/getAllContainerRepositories.json").toString().trim()).repositories
+// console.log(repos[5])
+// const data = JSON.parse(readFileSync("/Users/jercle/git/azgo/testData/20220616/getsubAssessments.json").toString().trim()).subAssessments
+// console.log(data)
+
+
+// vulnerabilityFilter(transformVulnerabilityData(data, repos), ["os:windows", "patchable:false", "severity:medium"])
+
+export function vulnerabilityFilter(data, filter) {
+  const filters = filter.reduce((all, item, index) => {
+    let [attr, val] = item.split(':')
+    if (!all[attr]) {
+      all[attr] = []
+    }
+    all[attr] = val.split(',').map(v => {
+      if (v.toLowerCase() === 'true') {
+        return true
+      } else if (v.toLowerCase() === 'false') {
+        return false
+      } else {
+        return v.toLowerCase()
+      }
+    })
+    return all
+  }, {})
+
+  const filterKeys = Object.keys(filters);
+  const filtered = data.filter(function (eachObj) {
+    return filterKeys.every(function (eachKey) {
+      if (!filters[eachKey].length) {
+        return true;
+      }
+      return filters[eachKey].includes(eachObj[eachKey]);
+    });
+  });
+  // console.log(filtered)
+  return filtered
+}
+
+
+export async function uploadToMongoDatabase(data, { dbConnectionString }) {
+  const client = new MongoClient(dbConnectionString);
+  try {
+    console.log(`Uploading ${chalk.yellow(data.length)} vulnerabilities to database`)
+
+    await client.connect();
+    const database = client.db("client-dawe-azgo-db")
+    const vulns = database.collection("client-dawe-azgo-vulnerabilities")
+
+    let bulk = vulns.initializeUnorderedBulkOp()
+    data.forEach(async (vuln) => {
+
+      await bulk.find({ _id: vuln._id }).upsert().replaceOne(vuln)
+    })
+    const bulkExecution = await bulk.execute()
+    console.log(bulkExecution)
+
+  } catch (err) {
+    console.log(err)
+
+  } finally {
+
+    await client.close();
+  }
+}
+
+// getTrivyDownload()
+export async function getTrivyDownload() {
+  const data = await (await fetch('https://api.github.com/repos/aquasecurity/trivy/releases/latest')).json()
+  // console.log(data)
+  const plat = platform() === 'darwin' ? 'macos' : platform()
+  const architecture = arch() === 'amd64' ? '64bit' : arch()
+  const filter = [plat, architecture]
+  // console.log(filter)
+  // only show browser_download_url attributes
+  const download = data.assets.filter(item => filter.every(f => item.browser_download_url.toLowerCase().includes(f)))
+  // const download = data.assets.filter(item => item.browser_download_url.toLowerCase().includes(filter))
+  const url = download.map(i => `${i.browser_download_url}\n`)
+
+  if (plat === 'macos') {
+    console.log(`${chalk.bold(chalk.underline('There are multiple ways in which you can install trivy'))}
+${chalk.underline('1. Homebrew:')}
+${chalk.dim('brew install aquasecurity/trivy/trivy')}
+${chalk.underline("2. Using AquaSec's install script:")}
+${chalk.dim('curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3')}
+${chalk.underline('3. Download the appropriate binary and add it to your path:')}
+${chalk.dim(url.join('').trim())}
+3. Or build from source - see https://aquasecurity.github.io/trivy/v0.18.3/installation/ for more information`)
+} else {
+  console.log(`${chalk.bold(chalk.underline('There are multiple ways in which you can install trivy'))}
+  ${chalk.underline("1. Using AquaSec's install script:")}
+  ${chalk.dim('curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3')}
+  ${chalk.underline('2. Download the appropriate binary and add it to your path:')}
+  ${chalk.dim(url.length > 0 ? url.join('').trim() : 'https://api.github.com/repos/aquasecurity/trivy/releases/latest')}
+3. Or build from source - see https://aquasecurity.github.io/trivy/v0.18.3/installation/ for more information`)
+  }
+}
+// const exists = commandExists.sync('trivy')
+// console.log(exists)
+
+// https://aquasecurity.github.io/trivy/v0.18.3/installation/#install-script
+// curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3
+// arch:
+// 'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', and 'x64'
+// platform:
+// 'aix', 'darwin', 'freebsd','linux', 'openbsd', 'sunos', and 'win32'.
